a6f07ec7da7e90c0974204adf699379c60fcf429
Chrabon Dey Sarker <chrabondey@gmail.com>
2025-03-10 19:45:16 +0600

    here i just add a things

---
+++ src/Pages/Earth/Earth.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Spline from '@splinetool/react-spline';
+
+const Earth = () => {
+    return (
+     
+        <Spline scene="https://prod.spline.design/diJ7o9IaxVOgwQsW/scene.splinecode" />
+    );
+  
+
+};
+
+export default Earth;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -4,12 +4,14 @@ import Skills from '../Skills/Skills';
 import Education from '../Education/Education';
 import Projects from '../projects/Projects';
 import Contact from '../Contact/Contact';
+import Earth from '../Earth/Earth';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
             <Skills></Skills>
+            <Earth></Earth>
             <Education></Education>
             <Projects></Projects>
             <Contact></Contact>
